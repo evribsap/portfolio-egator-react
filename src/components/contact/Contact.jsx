6944c78520b02682a67ce2cb9a246b9f55bd9021
evribsap <evribsaputra@gmail.com>
2022-07-31 01:21:26 +0700

    initial commit

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Contact.css'
+
+const contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default contact
---
+++ src/components/nav/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navigation.css'
+
+const Navigation = () => {
+  return (
+    <div>Navigation</div>
+  )
+}
+
+export default Navigation
