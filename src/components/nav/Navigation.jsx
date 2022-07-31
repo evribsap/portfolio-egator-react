a90dfcea1e0fac55914ffbd6ceebf482452e4717
evribsap <evribsaputra@gmail.com>
2022-08-01 00:50:55 +0700

    add nav portfolio

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -3,8 +3,9 @@ import './Navigation.css'
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
 import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
-import {useState} from 'react'
+import {BsFiles} from 'react-icons/bs'
 
+import {useState} from 'react'
 
 const Navigation = () => {
 
@@ -16,6 +17,7 @@ const [activeNav, setActiveNav] = useState('#home')
        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
        <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
        <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+       <a href="#portfolio" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><BsFiles/></a>
        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
 
     </nav>
