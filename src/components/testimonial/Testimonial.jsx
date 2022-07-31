5bddf5bdbd5bc9c746d5bf5e7322b476b5244a68
evribsap <evribsaputra@gmail.com>
2022-08-01 00:46:39 +0700

    update page testimonials

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './Testimonial.css'
+import { TestimonialData } from './TestimonialData'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Testimonial = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>
+        Review from clients
+      </h5>
+      <h2>
+        Testimonial
+      </h2>
+      <Swiper 
+      className="container testimonials__container"
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{
+        dynamicBullets: true,
+      }}
+      >
+        {
+          TestimonialData.map(({avatar, name, review}, index) => {
+            return (
+              <SwiperSlide key={index} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} alt={name} />
+                </div>
+                <h5 className="client__name">
+                  {name}
+                </h5>
+                <small className="client__review">
+                  {review}
+                </small>
+              </SwiperSlide>
+            )
+          })
+        }
+        
+      </Swiper>
+    </section>
   )
 }
 
---
+++ src/components/testimonial/TestimonialData.jsx
@@ -0,0 +1,27 @@
+import Client1 from '../../assets/avatar1.jpg'
+import Client2 from '../../assets/avatar2.jpg'
+import Client3 from '../../assets/avatar3.jpg'
+import Client4 from '../../assets/avatar4.jpg'
+
+export const TestimonialData = [
+    {
+        avatar: Client1,
+        name: 'Client 1',
+        review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus laborum veniam eius mollitia exercitationem dolorum itaque accusantium optio harum similique!'
+    },
+    {
+        avatar: Client2,
+        name: 'Client 2',
+        review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus laborum veniam eius mollitia exercitationem dolorum itaque accusantium optio harum similique!'
+    },
+    {
+        avatar: Client3,
+        name: 'Client 3',
+        review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus laborum veniam eius mollitia exercitationem dolorum itaque accusantium optio harum similique!'
+    },
+    {
+        avatar: Client4,
+        name: 'Client 4',
+        review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus laborum veniam eius mollitia exercitationem dolorum itaque accusantium optio harum similique!'
+    },
+]
