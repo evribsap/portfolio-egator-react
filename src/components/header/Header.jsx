ff6f4c42985a5fb456c120e52d644961bf5699f9
evribsap <evribsaputra@gmail.com>
2022-07-31 13:00:50 +0700

    update css and header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSosial from './HeaderSosial'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>
+          Hello I'm
+        </h5>
+        <h1>
+          Hajia Bintu
+        </h1>
+        <h5 className="text-light">
+          Fullstack Developer
+        </h5>
+        <CTA />
+        <HeaderSosial />
+        <div className="me">
+          <img scr={ME} alt="Hajia Bintu"/>
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        </div>
+    </header>
   )
 }
 
