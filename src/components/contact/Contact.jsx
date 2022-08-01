7e59040636b5ed239d01039c95d0749a2d249691
evribsap <evribsaputra@gmail.com>
2022-08-01 20:05:59 +0700

    add page contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
-import './Contact.css'
+import React from 'react';
+import './Contact.css';
+import {MdOutlineEmail} from 'react-icons/md';
+import {RiMessengerLine} from 'react-icons/ri';
+import {BsWhatsapp} from 'react-icons/bs';
+import { useRef as UseRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
+
 
 const contact = () => {
+  const form = UseRef("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
+
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>
+        Get In Touch
+      </h5>
+      <h2>
+        Contact Me
+      </h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon"/>
+            <h4>
+             Email
+            </h4>
+            <h5>
+              example@gmail.com
+            </h5>
+            <a href="mailto:example@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon"/>
+            <h4>
+             Messenger
+            </h4>
+            <h5>
+              example
+            </h5>
+            <a href="https://example.com" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon"/>
+            <h4>
+             WhatsApp
+            </h4>
+            <h5>
+              123-4567-8900
+            </h5>
+            <a href="https://wa.me/123-4567-8900" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required/>
+          <input type="email" name="email" placeholder="Your Email" required/>
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
